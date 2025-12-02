be79f1aa87e8b60a45fddb20010a0185f3c45c18
Leonardo Reblin <leoreblin@gmail.com>
2025-12-02 00:39:10 -0300

    fix: configurar swagger doc e annotations

--- src/CreditoApi/DependencyInjection.cs
+++ src/CreditoApi/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Confluent.Kafka;
 using CreditoApi.Infrastructure;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
 
 namespace CreditoApi;
 
@@ -9,7 +10,16 @@ public static class DependencyInjection
     public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Credito API",
+                Version = "v1",
+                Description = "API para integração e consulta de créditos constituídos."
+            });
+            options.EnableAnnotations();
+        });
         services.AddControllers();
         services.AddExceptionHandler<GlobalExceptionHandler>();
         services.AddProblemDetails();
